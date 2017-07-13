aed422861841d84ae015c505900dd435b19607d2
Arthur Burkart <artburkart@gmail.com>
2017-07-12 22:28:01 -0400

    Version 1.0.3: Sleeps until cards are present
    Now the extension sleeps until cards are present, then attempts to
    change their background. Additionally, the cards will be affected after
    a page click or mouseup event happens.

--- agile.js
+++ agile.js
@@ -1,20 +1,27 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://ssl.google-analytics.com/analytics.js','ga');
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-ga('create', 'UA-52476275-4', 'auto');
-
-[].forEach.call(document.getElementsByClassName('ghx-issue'), (el, idx) => {
-  // Only track for first page load
-  if (idx === 0) {
-    ga('send', 'pageview');
+// Sets background color on issues when "card color" is available
+async function changeColors() {
+  while (!document.getElementsByClassName('ghx-issue').length) {
+    await sleep(500);
   }
+  // Iterate through cards and change background color
+  [].forEach.call(document.getElementsByClassName('ghx-issue'), (el, idx) => {
+    // Only change color when color is set.
+    let grabber = el.children[2] || {style: {}};
+    let bg = grabber.style['background-color'] || '';
+    if (bg.replace(/ /g,'') !== '') {
+      let rgb = tinycolor(bg).lighten(30).toRgbString();
+      el.style.backgroundColor = rgb;
+    }
+  });
+}
 
-  // Only change color when color is set.
-  let bg = el.children[2].style['background-color'];
-  if (bg.replace(/ /g,'') !== '') {
-    let rgb = tinycolor(bg).lighten(30).toRgbString();
-    el.style.backgroundColor = rgb;
-  }
-});
+// Start on page load
+changeColors();
+
+// Try to set whenever mouse events happen
+document.onclick = changeColors;
+document.onmouseup = changeColors;
